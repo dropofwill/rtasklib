f1e2341a00a1ce93dd5d9c42271997bcc5082449
Will <whp3652@rit.edu>
2015-03-15 21:37:54 -0400

    Require new files

--- lib/rtasklib.rb
+++ lib/rtasklib.rb
@@ -1,6 +1,19 @@
 require_relative "rtasklib/version"
-require_relative "rtasklib/marshallable"
 require_relative "rtasklib/models"
+require_relative "rtasklib/execute"
+require_relative "rtasklib/controller"
+require_relative "rtasklib/serializer"
+require_relative "rtasklib/rc"
+
+# deprecated
+require_relative "rtasklib/marshallable"
 
 module Rtasklib
+
+  class TaskWarrior
+
+    def initialize rc="#{Dir.home}/.taskrc"
+
+    end
+  end
 end
