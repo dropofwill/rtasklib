206c656bc182aeda3e21cf44c7f0de66b69fcf8b
Will <whp3652@rit.edu>
2015-03-16 01:04:21 -0400

    Add version checking, with warning

--- lib/rtasklib.rb
+++ lib/rtasklib.rb
@@ -11,8 +11,17 @@ require_relative "rtasklib/marshallable"
 module Rtasklib
 
   class TaskWarrior
-    def initialize rc="#{Dir.home}/.taskrc"
+    attr_reader :taskrc, :version
 
+    def initialize rc="#{Dir.home}/.taskrc"
+      # Need to check TW version
+      @version = `task _version`.chomp
+
+      if Gem::Version.new(@version) < Gem::Version.new('2.4.0')
+        warn "#{@version} is untested"
+      end
+
+      # @taskrc = 
     end
   end
 end
