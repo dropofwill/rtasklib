63641de19753490bbc9e3a7ee25414587eccc2e3
Will <whp3652@rit.edu>
2015-03-16 02:13:05 -0400

    Add some error catching logic to the version checking call

--- lib/rtasklib.rb
+++ lib/rtasklib.rb
@@ -10,19 +10,33 @@ require "open3"
 module Rtasklib
 
   class TaskWarrior
-    attr_reader :taskrc, :version
+    attr_reader :taskrc, :version, :rc_location, :data_location
 
     def initialize rc="#{Dir.home}/.taskrc"
-      # Check TW version, and throw warning
-      raw_version = Open3.capture2("task _version")
-      @version = Gem::Version.new(raw_version[0].chomp)
-      # @version = Gem::Version.new(`task _version`.chomp)
+      @rc_location = rc
+      # TODO: use taskrc
+      @data_location = rc.chomp('rc')
 
-      if @version < Gem::Version.new('2.4.0')
-        warn "#{@version} is untested"
+      # Check TW version, and throw warning
+      begin
+        @version = check_version
+      rescue
+        warn "Couldn't find the task version"
       end
 
       # @taskrc =
+    end
+
+    private
+    def check_version
+      raw_version = Open3.capture2(
+        "task rc.data.location=#{@data_location} _version")
+      gem_version = Gem::Version.new(raw_version[0].chomp)
+
+      if gem_version < Gem::Version.new('2.4.0')
+        warn "#{@version} is untested"
+      end
+      gem_version
     end
   end
 end
