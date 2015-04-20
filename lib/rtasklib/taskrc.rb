3ba03324f466acacd7af9b60af7f0ca24d10dbdb
Will <whp3652@rit.edu>
2015-04-19 22:54:31 -0400

    Explicit public/private distinction for taskrc.rb, better documentation

--- lib/rtasklib/taskrc.rb
+++ lib/rtasklib/taskrc.rb
@@ -36,6 +36,7 @@ module Rtasklib
     #
     # @param taskrc_hash [Hash{Symbol=>String}]
     # @return [Models::TaskrcModel] the instance variable config
+    # @api private
     def hash_to_model taskrc_hash
       taskrc_hash.each do |attr, value|
         add_model_attr(attr, value)
@@ -43,12 +44,14 @@ module Rtasklib
       end
       config
     end
+    private :hash_to_model
 
     # Converts a .taskrc file path into a Hash that can be converted into a
     # TaskrcModel object
     #
     # @param rc_path [String,Pathname] a valid pathname to a .taskrc file
     # @return [Models::TaskrcModel] the instance variable config
+    # @api private
     def file_to_model rc_path
       taskrc = Hash[File.open(rc_path).map do |l|
         line_to_tuple(l)
@@ -56,12 +59,14 @@ module Rtasklib
 
       hash_to_model(taskrc)
     end
+    private :file_to_model
 
     # Converts a line of the form "json.array=on" to [ :json_array, true ]
     #
     # @param line [String] a line from a .taskrc file
     # @return [Array<Symbol, Object>, nil] a valid line returns an array of
     #   length 2, invalid input returns nil
+    # @api private
     def line_to_tuple line
       line = line.chomp.split('=', 2)
 
@@ -72,17 +77,13 @@ module Rtasklib
         return nil
       end
     end
+    private :line_to_tuple
 
-    def model_to_s
-      model_to_rc().join(" ")
-    end
-
-    def part_of_model_to_s *attrs
-      part_of_model_to_rc(*attrs).join(" ")
-    end
-
-    # Serialize the given attrs model back to taskrc format
-    # If attrs is nil, then default to all attributes
+    # Serialize the given attrs model back to the taskrc format
+    #
+    # @param attrs [Array] a splat of attributes
+    # @return [Array<String>] an array of CLI formatted strings
+    # @api public
     def part_of_model_to_rc *attrs
       attrs.map do |attr|
         value = get_model_attr_value attr
@@ -91,14 +92,43 @@ module Rtasklib
       end
     end
 
+    # Serialize all attrs of the model to the taskrc format
+    #
+    # @return [Array<String>] an array of CLI formatted strings
+    # @api public
     def model_to_rc
       part_of_model_to_rc(*config.attributes.keys)
+    end
+
+    # Serialize the given attrs model back to the taskrc format and reduce them
+    # to a string that can be passed directly to Execute
+    #
+    # @param attrs [Array] a splat of attributes
+    # @return [String] a CLI formatted string
+    # @api public
+    def part_of_model_to_s *attrs
+      part_of_model_to_rc(*attrs).join(" ")
+    end
+
+    # Serialize all attrs model back to the taskrc format and reduce them
+    # to a string that can be passed directly to Execute
+    #
+    # @return [String] a CLI formatted string
+    # @api public
+    def model_to_s
+      model_to_rc().join(" ")
     end
 
     # Dynamically add a Virtus attr, detect Boolean, Integer, and Float types
     # based on the value, otherwise just treat it like a string.
     # Int needs to precede float because ints are also floats
+    # Modifies the config instance variable
     # TODO: May also be able to detect arrays
+    #
+    # @param attr [#to_sym] the name for the attr, e.g. "json_array"
+    # @param value [String] the value of the attr, e.g. "yes"
+    # @return [undefined]
+    # @api private
     def add_model_attr attr, value
       if boolean? value
         config.attribute attr.to_sym, Axiom::Types::Boolean
@@ -110,26 +140,48 @@ module Rtasklib
         config.attribute attr.to_sym, String
       end
     end
+    private :add_model_attr
 
+    # Modifies the value of a given attr in the config object
+    #
+    # @param attr [#to_s] the name for the attr, e.g. "json_array"
+    # @param attr [String] the value of the attr, e.g. "yes"
+    # @return [undefined]
+    # @api public
     def set_model_attr_value attr, value
       config.send("#{attr}=".to_sym, value)
     end
 
+    # Gets the current value of a given attr in the config object
+    #
+    # @param attr [#to_s] the name for the attr, e.g. "json_array"
+    # @return [Object] the type varies depending on the type attr
+    # @api public
     def get_model_attr_value attr
       config.send("#{attr.to_s}".to_sym)
     end
 
-
-    private
+    # @param attr [String] the name for the attr, e.g. "json_array"
+    # @return [String]
+    # @api private
     def get_hash_attr_from_rc attr
       return attr.gsub(".", "_")
     end
+    private :get_hash_attr_from_rc
 
+    # @param attr [String] the name for the attr, e.g. "json_array"
+    # @return [String]
+    # @api private
     def get_rc_attr_from_hash attr
       return attr.gsub("_", ".")
     end
+    private :get_rc_attr_from_hash
 
-
+    # Check whether a given object is a path and it exists on the file system
+    #
+    # @param path [Object]
+    # @return [Boolean]
+    # @api private
     def path_exist? path
       if path.is_a? Pathname
         return path.exist?
@@ -139,18 +191,21 @@ module Rtasklib
         return false
       end
     end
-
+    private :path_exist?
 
     def integer? value
       value.to_i.to_s == value
     end
+    private :integer?
 
     def float? value
       Float(value) rescue false
     end
+    private :float?
 
     def boolean? value
       ["on", "off", "yes", "no", "false", "true"].include? value.to_s.downcase
     end
+    private :boolean?
   end
 end
